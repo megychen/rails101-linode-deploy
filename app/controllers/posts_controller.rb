7d51c47ca22627a9e0a6c9ad734c9dc7976fc5f9
megy chen <megy.chen203@gmail.com>
2017-01-01 12:05:57 +0800

    implement my post's edit and destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
+  before_action :find_post_group, :only => [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
@@ -19,7 +23,26 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:alert] = "Post Deleted"
+    redirect_to account_posts_path
+  end
+
   private
+
+  def find_post_group
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
